b67a6add170b291502c13d12ce3f0fd9ed3d1f7f
Ryan <95334241+ryanradtke03@users.noreply.github.com>
2025-05-04 22:35:13 -0700

    fix: update routing and redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Dashboard from "./pages/Dashboard";
 import HabitDetail from "./pages/HabitDetail";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/habit/:habitName" element={<HabitDetail />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/habit/:habitName" element={<HabitDetail />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
   );
 }
 
